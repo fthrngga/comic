21381d12397d3aeb18faa29213b0e839e8230a92
fthrngga <fathur22ti@mahasiswa.pcr.ac.id>
2026-09-08 16:09:49 +0700

    fix: download composer.phar in render build script

--- render-build.sh
+++ render-build.sh
@@ -3,8 +3,24 @@
 # Exit on error
 set -o errexit
 
+echo "==> Downloading Composer..."
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid composer installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+rm composer-setup.php
+# composer.phar sekarang ada di direktori saat ini
+
 echo "==> Installing PHP dependencies..."
-composer install --no-dev --optimize-autoloader
+php composer.phar install --no-dev --optimize-autoloader
 
 echo "==> Installing Node.js dependencies..."
 npm ci
